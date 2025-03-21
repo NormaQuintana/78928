94bb851e252ad5f1bc8d268c677d14d71b3fa881
NormaQuintana <NormaQuintana@gmail.com>
2025-03-21 10:55:18 -0600

    flask03

---
+++ flask03/rutas.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+rutas_bp = Blueprint('rutas', __name__, url_prefix='/api/v1')
+
+#se define dos rutas en el blueprint
+@rutas_bp.route('/ruta1')
+def ruta1():
+    return 'Esta es la Ruta 1'
+
+@rutas_bp.route('/ruta2')
+def ruta2():
+    return 'Esta es la Ruta 2'
